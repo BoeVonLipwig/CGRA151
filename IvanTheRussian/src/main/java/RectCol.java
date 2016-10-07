3122582e9aaa94a68c1837746c7f9d0861dd48ad
sdmsi <sdmsinclair@gmail.com>
2016-10-07 15:51:36 +1300

    draw from images

--- IvanTheRussian/src/main/java/RectCol.java
+++ IvanTheRussian/src/main/java/RectCol.java
@@ -3,7 +3,7 @@
  * CGRA 151.
  * Start date: 27/09/2016.
  */
-public class RectCol {
+class RectCol {
     private static double size = IvanTheRussian.size;
 
     static Blocks collisionIvan(Ivan ivan, Blocks rect) {
@@ -30,27 +30,4 @@ public class RectCol {
         return null;
     }
 
-//    double x = ivan.getPosition().x;
-//    double y = ivan.getPosition().y;
-//    double rectY=rect.getPos().y,rectX=rect.getPos().x;
-//    int left = 1, right = 2, top = 3, bottom = 4, noCollide = 0;
-//    //0=no collision 1=left 2=right 3=top 4=bottom
-//        if (ivan.getAcl().y > 0 && y >= rectY && x > rectX && x < rectX + size) {
-//        IvanTheRussian.setJumpAllowed(true);
-//        return rect;
-//    }
-//        if (ivan.getAcl().x > 0 && x >= rectX && y > rectY && y < rectY + size) {
-//        return rect;
-//    }
-//
-//
-//        if (ivan.getAcl().x < 0 && x <= rectX + size && y > rectY && y < rectY + size) {
-//        return rect;
-//    }
-//
-//
-//        if (ivan.getAcl().y < 0 && y <= rectY - size && x > rectX && x < rectX + size) {
-//        return rect;
-//    }
-//        return null;
 }
